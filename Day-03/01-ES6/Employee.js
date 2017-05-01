a105c103ef0debb51590f7e9afcf92f8300c4866
Magesh <tkmagesh77@gmail.com>
2017-05-01 14:13:23 +0530

    Bug Trucker - Initial

--- Day-03/01-ES6/Employee.js
+++ Day-03/01-ES6/Employee.js
@@ -1,19 +1,23 @@
-class Employee{
-	constructor(id, name, salary){
-		this.__id = id;
-		this.name = name;
-		this.salary = salary;
-    }
-	set id(value){
-		console.log('id setter triggered');
-		if (value < 0) return;
-		this.__id = value;
+var Employee = (function(){
+	let idSymbol = Symbol();
+	class Employee{
+		constructor(id, name, salary){
+			this[idSymbol] = id;
+			this.name = name;
+			this.salary = salary;
+	    }
+		set id(value){
+			console.log('id setter triggered');
+			if (value < 0) return;
+			this[idSymbol] = value;
+		}
+		get id(){
+			console.log('id getter triggered');
+			return this[idSymbol];
+	    }
+		display(){
+			console.log(`id = ${this.id}, name = ${this.name}, salary = ${this.salary}`);
+		}
 	}
-	get id(){
-		console.log('id getter triggered');
-		return this.__id;
-    }
-	display(){
-		console.log(`id = ${this.id}, name = ${this.name}, salary = ${this.salary}`);
-	}
-}
+	return Employee;
+})()
